8be879ab3ee3c370995eeb792f5d98a5b732106b
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2021-07-05 21:17:13 -0300

    FRONT completo

--- apicomputador/src/models/ComputadorSchema.ts
+++ apicomputador/src/models/ComputadorSchema.ts
@@ -4,28 +4,28 @@ const computadorSchema = new Schema(
     {
         dono:{
             type: String,
-            required:[true, "O campo nome é obrigatório"]
+            required:[true, "O campo dono é obrigatório"]
         },
         placaMae:{
             type:String,
-            required:[true, "O campo ISBN é obrigatório"]
+            required:[true, "O campo placa mãe é obrigatório"]
             
         },
         processador:{
             type: String,
-            required:[true, "O campo nome é obrigatório"]
+            required:[true, "O campo processador é obrigatório"]
         },
         memoria:{
             type: Number,
-            required:[true, "O campo preço é obrigatório"]
+            required:[true, "O campo memoria é obrigatório"]
         },
         armazenamento:{
             type: String,
-            required:[true, "O campo nome é obrigatório"]
+            required:[true, "O campo armazenamento é obrigatório"]
         },
         fonte:{
             type: String,
-            required:[true, "O campo nome é obrigatório"]
+            required:[true, "O campo fonte é obrigatório"]
         },
         
     },
